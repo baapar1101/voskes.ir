7a5f9d86cac2fa38c89ce949202aa57bc7a535bc
Albert Heidarian <50908145+baapar1101@users.noreply.github.com>
2026-08-18 20:40:06 +0330

    ir

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://voskes.nl',
-	trailingSlash: 'never',
-	build: {
-		format: 'preserve',
-	},
+vite: {
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: [
+        'voskes.ir',
+        'www.voskes.ir'
+      ]
+    }
+  }
 });
