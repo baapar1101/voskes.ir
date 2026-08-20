9b647a98e4faab88b9b154cbf6d4a8d55fa5bfd3
Albert Heidarian <50908145+baapar1101@users.noreply.github.com>
2026-08-20 15:23:01 +0330

    Refactor links in dog treats and training treats pages to remove .html extensions for cleaner URLs; update navigation and footer links accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: 'never',
   build: {
-    format: 'file',
+    format: 'directory',
   },
 
   vite: {
